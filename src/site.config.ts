24d61ba2029ac9bb32defdaa88d44856e5c928ac
samtdevelops <samthomas2108@gmail.com>
2026-07-03 23:30:26 +0100

    feat: implement main pages

---
+++ src/site.config.ts
@@ -0,0 +1,8 @@
+export const site = {
+  name: "sam thomas",
+  url: "https://samthomas.dev",
+  email: "hello@samthomas.dev",
+  linkedin: "https://www.linkedin.com/in/samtdev",
+  github: "https://github.com/samtdevelops",
+  careerStart: "2024-04-16",
+} as const;
