09c4d2d4c7cbd15d1d7da656515d70ec95e94eb3
samtdevelops <samthomas2108@gmail.com>
2026-07-17 00:11:51 +0100

    chore: update configuration and enhance styles and content on main pages

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,5 +4,4 @@ export const site = {
   email: "hello@samthomas.dev",
   linkedin: "https://www.linkedin.com/in/samtdev",
   github: "https://github.com/samtdevelops",
-  careerStart: "2024-04-16",
 } as const;
